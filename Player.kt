1906b22e4c1cadfa757d4d09c4cf5b4fb49aeaf8
shinre <ex559630@gmail.com>
2021-06-28 21:58:41 +0800

    c13-1

---
+++ Player.kt
@@ -0,0 +1,38 @@
+class Player (_name:String,
+            _healthPoints:Int,
+            _isBlessed:Boolean,
+            _isImmortal:Boolean){
+    var name = _name
+        get() = field.capitalize()
+        private set(value) {
+            field = value.trim()
+        }
+    val healthPoints = _healthPoints
+    var isBlessed = _isBlessed
+    private var isImmortal = _isImmortal
+
+    fun auraColor():String{
+        var auraVisible = healthPoints >=50 && isBlessed || isImmortal
+        val auracolor = if (auraVisible) "Green" else "None"
+        return auracolor
+    }
+    fun formatHealthStatus() = when(healthPoints){
+        100 -> "is in excellent condition!"
+        in 90..99 -> "has a few scratches."
+        in 75..89 -> if (isBlessed){
+            "has some minor wounds but is heading quite quickly."
+        }else {
+            "has some minor wounds."
+        }
+        in 15..74 -> "look pretty hurt."
+        else -> "is in awful condition"
+    }
+    fun castFireball(numFireballs:Int=2){
+        println("A glass of FireBall springs into existence.(x$numFireballs)")
+    }
+}
+/*
+field 不允許直接定義
+get 讀取屬性
+set 設定屬性
+ */
